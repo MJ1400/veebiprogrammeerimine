158955688a028ab6aca3b5b40476ab8df235f713
“Marten <MJ1400@users.noreply.github.com>
2019-09-16 11:30:01 +0300

    Kodutöö, lisatud 3 if-lauset

--- tund2/try.php
+++ tund2/try.php
@@ -5,6 +5,12 @@
   $partOfDay = "hägune aeg";
   if($hourNow < 8) {
     $partOfDay = "varane hommik";
+  } elseif($hourNow > 8 and $hourNow < 12) {
+      $partofDay = "Hommik";
+  } elseif($hourNow > 12 and $hourNow < 16) {
+      $partofDay = "Lõuna";
+  } elseif($hourNow > 16 and $hourNow < 20) {
+      $partofDay = "Õhtu";
   }
 ?>
 
